66a9f9f9bd07dfb0e4f8de6816e841dc0bde7931
simonbourrasseau <simon.bourrasseau@imie.fr>
2018-12-04 12:13:24 +0100

    créer index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+class person { 
+	private $firstname;
+	private $lastname;
+
+	public function setFirstname($pFirstname) {
+		$this->firstname = $pFirstname;
+	}
+
+	public function getFirstname() {
+		return $this->firstname;
+	}
+
+	public function setLastname($pLastname) {
+		$this->lastname = $pLastname;
+	}
+
+	public function getLastname() {
+		return $this->lastname;
+	}
+
+	public function speak() {
+		return "Hello my name is" . $this->firstname . " " . $this->lastname;
+	}
+}
+
+$person1 = new Person();
+
+//$person1->firstname = "John";
+//$person1->lastname = "Doe";
+
+
+$person1->setFirstname("John");
+$person1->setLastname("Doe");
+
+var_dump($person1->speak());
+
+//var_dump($person1->getFirstname());
+//var_dump($person1->getLastname());
+
+//$person2 = new person();
+//$person2->firstname = "Jane";
+//$person2->lastname = "die";
+//$person2->setFirstname("Jane");
+//$person2->setLastname("Die");
+
+//var_dump($person2);
